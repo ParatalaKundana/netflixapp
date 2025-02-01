bed715d4edd7a0058d8f1c39013f3951d2f49853
ParatalaKundana <133763044+ParatalaKundana@users.noreply.github.com>
2025-02-01 15:43:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ count = 5
 ami = "ami-02972a2a0ac299bb7"
 instance_type = "t2.medium"
 key_name = "kundu"
-vpc_security_group_ids = ["sg-078079fbc468cc057"]
+vpc_security_group_ids = ["sg-0e31c2d5c5a74b17d"]
 tags = {
 Name = var.instance_names[count.index]
 }
